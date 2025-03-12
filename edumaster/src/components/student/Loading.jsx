10b1ecfc42355c989c82bbcef5c64a9e6028746e
AMAN <amanshn2001@gmail.com>
2025-03-12 22:59:47 +0530

    initial commit

---
+++ edumaster/src/components/student/Loading.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+const loading = () => {
+  return (
+    <div>
+      <h1>Loading</h1>
+    </div>
+  )
+}
+
+export default loading
---
+++ edumaster/src/context/AppContext.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { createContext } from 'react';
+
+export const AppContext = createContext();
+export const AppContextProvider = (props)=>{
+
+    const value = {
+
+
+
+    }
+    
+    return (
+        <AppContext.Provider value = {value}>
+            {props.children}
+        </AppContext.Provider>
+    )
+};
+
+
+// 
+// () => {
+//   return (
+//     <div>
+      
+//     </div>
+//   )
+// }
+
+// export default AppContext
